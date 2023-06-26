e3b6e0e7dc7aa3edf24e9be6a1d1ed72a7b0b7f4
Vaibhav Ramchandra Desai <127377578+Vaibhavdesai99@users.noreply.github.com>
2023-06-26 19:26:17 +0530

    Prerendering the powers of Next JS

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,4 @@
 import MeetUpList from "../components/meetups/MeetupList";
-
-//  here if we use useEffect , useState to fetch Api initailly we store in empty array , so basically when componet render for first time , useEffect call
-// then then again component re-render during this second cycle we get data but in Next js as here we see server side the initial cycle is empty array []
-// so user call might see empty data that is bad user Experience , so
-// In Next JS we have  getStaticProps() {} this function get called before the component render for first time , so we  use this function instead of useState and useEffect()
-// if we do view source then we dont see data if we use useState and useEffect() caz initailly it is empty so by using this getStatis func we see that actual content of data.
-// In getStaticProps() has props property we pass that to our component function .
 const DUMMY_MEETUPS = [
   {
     id: "m1",
@@ -33,11 +26,6 @@ function HomePage(props) {
   );
 }
 
-// Data is not fetched at second  component render cycle on the  client , but initially before this page is pre-render during the built process : - this is one most feature : data fetching for pre-rendering
-// This function executed during pre-rendering process :-
-// this function get called first before the call the component function : job is to prepare PROP to this page . => This props contain the data that this page needs.
-// This function return promise ...async func so we have to wait until proimse get resolved ..so we have to wait till we get that data
-
 export async function getStaticProps() {
   // fetch data from API
   //  return must Object
@@ -51,8 +39,23 @@ export async function getStaticProps() {
   };
 }
 
+// ========  getServerSideProps()============>>
+
+// serverSideProp guranteed us to run at every req
+// This function run on the server after deployment
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from API
+//   // Any code we write here run on server not on client
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
 export default HomePage;
-
-// npm run build : -
-
-// revalidate: 5 => property : - whatever we set the no here , this page get ocssionaly be pre-generated on the server after deployment, so we dont have to re-deploy or ew-build just caz of some data change
